8a131d7d422c942d54d8e74f2a575d365e1962ff
brancori <brandoncortesr7793@gmail.com>
2022-04-25 21:15:21 -0500

    se agrega nuevos trabajos

---
+++ escuelajava/experiments.js
@@ -0,0 +1,30 @@
+//* Piedra = 1*//
+//* Papel = 2 *//
+//* Tijera= 3 *//
+
+var yo ="piedra";
+
+if (yo == "piedra"){
+    console.log("1")
+}
+if (yo == "papel"){
+    console.log("2")
+}
+else if (yo == "tijera"){
+    console.log("3")
+}
+
+var tu ="papel";
+
+if (yo == "piedra"){
+    console.log("1")
+}
+if (yo == "papel"){
+    console.log("2")
+}
+else if (yo == "tijera"){
+    console.log("3")
+}
+var tu ="papel";
+
+    
